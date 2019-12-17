e526e997da8f9c55b55bcda8c3468bfbfb47b42d
Ruchi makadia <ruchi.m@eheuristic.com>
2019-12-17 15:35:02 +0530

    Initial Commit

---
+++ ColorSlider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorSlider
+//
+//  Created by eHeuristic on 17/12/19.
+//  Copyright © 2019 eHeuristic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
